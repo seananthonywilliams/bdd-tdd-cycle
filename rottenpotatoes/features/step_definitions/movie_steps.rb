71b5c4596c82c3e4799e5ef73f303af711ac7b0a
Sean Williams <seananthonywilliams@gmail.com>
2014-06-06 02:29:46 -0300

    Added some movie step definitions with movie_steps.rb

---
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,11 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /^Given the following movies exist:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the director of "(.*?)" should be "(.*?)"$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
